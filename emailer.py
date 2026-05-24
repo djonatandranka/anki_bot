f61744315765f38b524823e8053053caf494734d
Djonatan Dranka <djonatan.dranka@gmail.com>
2026-05-24 13:47:06 +0200

    add configuration settings for deck and email notifications

--- emailer.py
+++ emailer.py
@@ -1,14 +1,27 @@
+import os
 import smtplib
+
 from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_TO = os.getenv("EMAIL_TO")
+
 
 def send_email(count):
     msg = EmailMessage()
-    msg["Subject"] = "Anki Updated"
-    msg["From"] = "you@gmail.com"
-    msg["To"] = "wife@gmail.com"
 
-    msg.set_content(f"{count} new German words added.")
+    msg["Subject"] = "Anki Vocabulary Updated"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = EMAIL_TO
+
+    msg.set_content(
+        f"{count} new German words were added to Anki."
+    )
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login("you@gmail.com", "APP_PASSWORD")
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
