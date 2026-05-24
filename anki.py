1475708112d2ed183a1bcc581f7f728b88f1524b
Djonatan Dranka <djonatan.dranka@gmail.com>
2026-05-24 13:07:26 +0200

    add anki.py with functions to invoke Anki API and manage vocabulary notes

---
+++ anki.py
@@ -0,0 +1,46 @@
+import requests
+
+ANKI_URL = "http://localhost:8765"
+
+def invoke(action, **params):
+    response = requests.post(
+        ANKI_URL,
+        json={
+            "action": action,
+            "version": 6,
+            "params": params
+        }
+    ).json()
+
+    if response.get("error"):
+        raise Exception(response["error"])
+
+    return response["result"]
+
+
+def add_word(
+    deck,
+    german,
+    portuguese,
+    sentence="",
+    tags=None
+):
+    if tags is None:
+        tags = []
+
+    note = {
+        "deckName": deck,
+        "modelName": "German Vocabulary",
+        "fields": {
+            "German": german,
+            "Portuguese": portuguese,
+            "Sentence": sentence
+        },
+        "tags": tags
+    }
+
+    return invoke("addNote", note=note)
+
+
+def sync():
+    return invoke("sync")
